971e17ef081638c7389d7fd98adc21ded0a7afc2
Cosmo André <cosmo_moraes@hotmail.com>
2019-06-04 17:01:40 -0300

    modificado migração nativa de users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,15 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name', 55);
             $table->string('email', 45)->unique();
+            $table->string('document', 14)->unique()
+                ->comment('aqui será o cpf');
+            $table->date('birth');
+            $table->string('phone', 15)->nullable();
+            $table->string('address', 150);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 60);
             $table->rememberToken(60);
+            $table->boolean('is_admin')->nullable();
             $table->softDeletesTz();
             $table->timestampsTz();
         });
